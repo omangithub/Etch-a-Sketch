d8bd89154e59c995952cb63a32c8868d32e25088
oman <smithowain@hotmail.com>
2024-08-26 23:32:03 +0900

    The project now runs with a button that changes the number of cells, but resizes them to fit the original size of container

--- javascript.js
+++ javascript.js
@@ -5,35 +5,49 @@ console.log("Hello World");
 const divSquares = document.getElementById("container");
 const gridSizeBtn = document.getElementById("sizeChangeBtn");
 
-
-console.log(divSquares)
-
 let rows = 16;
 let columns = 16;
+let gridMargin = rows+columns;
+let cellSize = 800;
 
- function divCreator () {
+/* function divCreator () {
+  console.log(rows);
+  document.getElementById("container").style.maxHeight=rows*50+gridMargin+"px";
+  document.getElementById("container").style.maxWidth=columns*50+gridMargin+"px";
     for (let c=0;c<rows*columns;c++) {
           let cell = document.createElement("div");
           cell.setAttribute("class", "cell");
           cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "red";});
+            cell.style.backgroundColor = "red";}); 
+        divSquares.appendChild(cell);
+    }
+};*/
+
+function divCreator () {
+  cellSize = 800/rows;
+  let cellSizeStr = cellSize-2 + "px";
+  console.log(cellSizeStr);
+    for (let c=0;c<rows*columns;c++) {
+          let cell = document.createElement("div");
+          cell.setAttribute("class", "cell");
+          cell.style.height =cellSizeStr;
+          cell.style.width =cellSizeStr;       
+          cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "red";}); 
         divSquares.appendChild(cell);
     }
 };
 
-divCreator();
-
-function isNumber(value) {
-  return typeof value === "number" && !Number.isNaN(value);
-}
-
 gridSizeBtn.addEventListener("click", ()=> {
-  let gridNumber = parseInt(window.prompt("What is your desired number of cells per side?", "Enter a number"));
-  console.log(typeof(gridNumber));
-  console.log(gridNumber);
-  if (isNaN(gridNumber) || gridNumber === 0) {
-    console.log("fail");
+  let gridNumber = parseInt(window.prompt("What is your desired number of cells per side?", "Enter a number between 1 and 100"));
+  if (isNaN(gridNumber) || gridNumber <= 0 || gridNumber>100) {
+    alert("You did not enter a number between 1 and 100");
   } else {
-    console.log("pass");
+    document.querySelectorAll(".cell").forEach(cell => cell.remove());
+    rows = gridNumber;
+    columns = rows;
+    divCreator();
   }
-})
+});
+
+divCreator();
