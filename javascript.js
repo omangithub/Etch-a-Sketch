590982955defd11d8e35c242b86e8241bbf941ad
oman <smithowain@hotmail.com>
2024-08-26 10:50:54 +0900

    Added a button with a prompt that accepts only numbers to use to create different sized grids in future commits

--- javascript.js
+++ javascript.js
@@ -3,36 +3,37 @@ console.log("Hello World");
 // create a grid of 16 by 16 div containers
 
 const divSquares = document.getElementById("container");
+const gridSizeBtn = document.getElementById("sizeChangeBtn");
 
 
 console.log(divSquares)
 
-const rows = 16;
-const columns = 16;
-
-/* function divCreator () {
-    for (let c=0;c<rows;c++) {
-        let row = document.createElement("div")
-        row.setAttribute("class", "rows");
-        for (let i=0;i<columns;i++) {
-          let cell = document.createElement("div");
-          cell.setAttribute("class", "cell");
-          cell.style.width = "25px";
-          cell.style.height = "25px";
-          cell.style.display = "inline-block";
-          row.appendChild(cell);
-        }
-        divSquares.appendChild(row);
-    }
-
-}*/
+let rows = 16;
+let columns = 16;
 
  function divCreator () {
     for (let c=0;c<rows*columns;c++) {
           let cell = document.createElement("div");
           cell.setAttribute("class", "cell");
+          cell.addEventListener("mouseover", () => {
+            cell.style.backgroundColor = "red";});
         divSquares.appendChild(cell);
     }
 };
 
 divCreator();
+
+function isNumber(value) {
+  return typeof value === "number" && !Number.isNaN(value);
+}
+
+gridSizeBtn.addEventListener("click", ()=> {
+  let gridNumber = parseInt(window.prompt("What is your desired number of cells per side?", "Enter a number"));
+  console.log(typeof(gridNumber));
+  console.log(gridNumber);
+  if (isNaN(gridNumber) || gridNumber === 0) {
+    console.log("fail");
+  } else {
+    console.log("pass");
+  }
+})
