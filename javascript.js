96d502bdf62607f08ef049b21c8c1e127f09529f
oman <smithowain@hotmail.com>
2024-08-27 09:32:03 +0900

    Cells now have random colors when the mouse passes over them

--- javascript.js
+++ javascript.js
@@ -10,33 +10,31 @@ let columns = 16;
 let gridMargin = rows+columns;
 let cellSize = 800;
 
-/* function divCreator () {
-  console.log(rows);
-  document.getElementById("container").style.maxHeight=rows*50+gridMargin+"px";
-  document.getElementById("container").style.maxWidth=columns*50+gridMargin+"px";
-    for (let c=0;c<rows*columns;c++) {
-          let cell = document.createElement("div");
-          cell.setAttribute("class", "cell");
-          cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "red";}); 
-        divSquares.appendChild(cell);
-    }
-};*/
+// a function that randomizes the colors of the cells unsing math floor to round down and math random to create a random number
+function r() {
+    return Math.floor(Math.random() * 256);
+}
+
+
+// A function that creates the grid
 
 function divCreator () {
   cellSize = 800/rows;
   let cellSizeStr = cellSize-2 + "px";
   console.log(cellSizeStr);
     for (let c=0;c<rows*columns;c++) {
+          let color = "rgb(" + r() + "," + r() + "," + r() + ")";
           let cell = document.createElement("div");
           cell.setAttribute("class", "cell");
           cell.style.height =cellSizeStr;
           cell.style.width =cellSizeStr;       
           cell.addEventListener("mouseover", () => {
-            cell.style.backgroundColor = "red";}); 
+            cell.style.backgroundColor = color;}); 
         divSquares.appendChild(cell);
     }
 };
+
+// a function that listens for click and grid number data from the user
 
 gridSizeBtn.addEventListener("click", ()=> {
   let gridNumber = parseInt(window.prompt("What is your desired number of cells per side?", "Enter a number between 1 and 100"));
